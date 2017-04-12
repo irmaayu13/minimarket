deaddb5969f1c2843718066cff1a3cbac22da135
irnandamaulidya <irnandaamaulidyaa@gmail.com>
2017-04-12 23:13:23 +0700

    Add files via upload

---
+++ bill.java
@@ -0,0 +1,12 @@
+/**
+ * Created by IRNANDA on 12/04/2017.
+ */
+public class bill {
+    String idpetugas;
+    String idproduk;
+    String namaproduk;
+    int kuantitas;
+    int harga;
+    String idmember;
+    int totalharga;
+}
