2cd4d8ff90e781292c7c462e782e11818d8c2a58
Rima Yuliana <rimayuliana17@student.upi.edu>
2017-05-01 17:11:12 +0700

    Dibuat  class baru untuk Detail fakturnya

---
+++ src/DetailFaktur.java
@@ -0,0 +1,42 @@
+import java.util.ArrayList;
+
+/**
+ *
+ */
+public class DetailFaktur {
+    import java.util.ArrayList;
+
+    public class detailFakturBeli extends FakturBeli {
+        String no_fakturbeli;
+        int jumlah_fakturbeli;
+        String tgl_masuk;
+        ArrayList<Produk> daftarProduk = new ArrayList<>();
+
+        public detailFakturBeli(String no_fakturbeli, int jumlah_fakturbeli, String tgl_masuk){
+            this.no_fakturbeli=no_fakturbeli;
+            this.jumlah_fakturbeli=jumlah_fakturbeli;
+            this.tgl_masuk=tgl_masuk;
+        }
+
+        @Override
+        public double totalFakturbeli() {
+            int total=0;
+
+            return total+jumlah_fakturbeli;
+
+        }
+
+
+        public void print() {
+            System.out.println("No. Faktur Beli : "+no_fakturbeli);
+            System.out.println("Jumlah Faktur Beli : "+jumlah_fakturbeli);
+            System.out.println("Tanggal Masuk : "+tgl_masuk);
+
+        }
+
+        public static void main(String[] args){
+            detailFakturBeli dfb = new detailFakturBeli("FB001",250,"02-04-2017");
+            dfb.print();
+        }
+    }
+}
